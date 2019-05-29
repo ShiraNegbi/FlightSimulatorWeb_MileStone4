6987c317d450d41eb73b2deaf6bd1af4f8922b62
Shira Negbi <shira.negbi@live.biu.ac.il>
2019-05-29 20:55:45 +0300

    Initial Commit

---
+++ Ajax-Minimal/Ajax-Minimal/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajax_Minimal.Models
+{
+    public class Location
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+}
