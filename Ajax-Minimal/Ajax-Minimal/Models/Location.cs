8691b518f1adef36711e64ce80743edddf45fd92
shay slonim <shayslonim@gmail.com>
2019-06-02 18:12:02 +0300

    Added ToXml to Location class

--- Ajax-Minimal/Ajax-Minimal/Models/Location.cs
+++ Ajax-Minimal/Ajax-Minimal/Models/Location.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace Ajax_Minimal.Models
 {
     public class Location
     {
-        public double Lon { get; set; }
         public double Lat { get; set; }
+        public double Lon { get; set; }
+
+        public void ToXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("Location");
+            writer.WriteElementString("Lat", this.Lat.ToString());
+            writer.WriteElementString("Lon", this.Lon.ToString());
+            writer.WriteEndElement();
+        }
     }
 }
