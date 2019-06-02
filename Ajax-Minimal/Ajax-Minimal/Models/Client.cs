848d62d6765e41f9572e3344a65ea7a71b449258
shay slonim <shayslonim@gmail.com>
2019-06-02 18:11:10 +0300

    Added Client.cs, a class that communicates with the server and can ask for parameters

---
+++ Ajax-Minimal/Ajax-Minimal/Models/Client.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Web;
+
+namespace Ajax_Minimal.Models
+{
+    public class Client
+    {
+        //a singleton - a single instance of the client
+        private static Client singletonClient = null;
+        //a property for getting the server instance from the class
+        public static Client Instance
+        {
+            get
+            {
+                if (singletonClient == null)
+                {
+                    Client.singletonClient = new Client();
+                }
+                return singletonClient;
+            }
+        }
+        public string DoMessage(string server, int port, string message)
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                TcpClient client = new TcpClient(server, port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+                return responseData;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+                throw e;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                throw e;
+            }
+
+            
+        }
+    }
+}
