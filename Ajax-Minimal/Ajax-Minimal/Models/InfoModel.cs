1069cc2fcfba65865495649333f760e74e66ca86
shay slonim <shayslonim@gmail.com>
2019-06-02 18:11:28 +0300

    Edited InfoModel to fit to Location

--- Ajax-Minimal/Ajax-Minimal/Models/InfoModel.cs
+++ Ajax-Minimal/Ajax-Minimal/Models/InfoModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
-
-namespace Ajax_Minimal.Models
+﻿namespace Ajax_Minimal.Models
 {
     public class InfoModel
     {
+
         private static InfoModel s_instace = null;
 
         public static InfoModel Instance
@@ -22,41 +17,23 @@ namespace Ajax_Minimal.Models
             }
         }
 
-        public Employee Employee { get; private set; }
+        public Location Location { get; private set; }
         public string ip { get; set; }
         public string port { get; set; }
         public int time { get; set; }
 
+        private LocationReadModel LocationReadModel { get; set; }
+
         public InfoModel()
         {
-            Employee = new Employee();
+            Location = new Location();
+            LocationReadModel = new LocationReadModel();
         }
 
-        public const string SCENARIO_FILE = "~/App_Data/{0}.txt";           // The Path of the Secnario
-
-        public void ReadData(string name)
+        public void ReadLocationData(string ip, int port)
         {
-            string path = HttpContext.Current.Server.MapPath(String.Format(SCENARIO_FILE, name));
-            if (!File.Exists(path))
-            {
-                Employee.FirstName = name;
-                Employee.LastName = name;
-                Employee.Salary = 500;
-
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(path, true))
-                {
-                    file.WriteLine(Employee.FirstName);
-                    file.WriteLine(Employee.LastName);
-                    file.WriteLine(Employee.Salary);
-                }
-            }
-            else
-            {
-                string[] lines = System.IO.File.ReadAllLines(path);        // reading all the lines of the file
-                Employee.FirstName = lines[0];
-                Employee.LastName = lines[1];
-                Employee.Salary = int.Parse(lines[2]);
-            }
+            LocationReadModel.ReadLocationData(ip, port);
+            Location = LocationReadModel.Location;
         }
 
     }
